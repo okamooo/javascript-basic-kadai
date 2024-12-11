b56378cb74eef7e5203edeb192615bb73ff25444
okamooo <okamo0180@gmail.com>
2024-12-11 18:41:35 +0900

    Dateクラスをアップロード

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,6 @@
+
+var currentDate = new Date();
+var year = currentDate.getFullYear();
+var month = currentDate.getMonth() + 1;
+var date = currentDate.getDate();
+console.log(year + "年" + month + "月" + date +"日"); 
